39547ec1866fcfc3a724196525b91c2a81466654
markkimjr <markkimjr96@gmail.com>
2025-04-11 14:34:18 +0900

    clean up fighter routes (use dependencies for service instantiation)

--- sbtb/fighter/dependencies.py
+++ sbtb/fighter/dependencies.py
@@ -1,33 +1,66 @@
+from fastapi import Depends
+
 from sbtb.database.session import DbSession
 from sbtb.fighter.scraper import BoxingRankScraper, BoxingFightCardScraper
+from sbtb.fighter.service import BoxerScraperService, BoxingFightCardService
 from sbtb.fighter.repository import FighterRepo, FightOrganizationRepo, RankRepo, WeightClassRepo, FightCardRepo
 from sbtb.fighter.driver import ChromeDriver
 
 
-async def get_boxing_scraper():
+def get_boxing_scraper():
     return BoxingRankScraper()
 
 
-async def get_boxing_fight_card_scraper() -> BoxingFightCardScraper:
+def get_boxing_fight_card_scraper() -> BoxingFightCardScraper:
     chrome_driver = ChromeDriver()
     return BoxingFightCardScraper(driver=chrome_driver)
 
 
-async def get_boxer_repo(db: DbSession) -> FighterRepo:
+def get_boxer_repo(db: DbSession) -> FighterRepo:
     return FighterRepo(db=db)
 
 
-async def get_boxer_org_repo(db: DbSession) -> FightOrganizationRepo:
+def get_boxer_org_repo(db: DbSession) -> FightOrganizationRepo:
     return FightOrganizationRepo(db=db)
 
 
-async def get_rank_repo(db: DbSession) -> RankRepo:
+def get_rank_repo(db: DbSession) -> RankRepo:
     return RankRepo(db=db)
 
 
-async def get_weight_class_repo(db: DbSession) -> WeightClassRepo:
+def get_weight_class_repo(db: DbSession) -> WeightClassRepo:
     return WeightClassRepo(db=db)
 
 
-async def get_fight_card_repo(db: DbSession) -> FightCardRepo:
+def get_fight_card_repo(db: DbSession) -> FightCardRepo:
     return FightCardRepo(db=db)
+
+
+def get_boxer_scraper_service(
+    scraper: BoxingRankScraper = Depends(get_boxing_scraper),
+    fighter_repo: FighterRepo = Depends(get_boxer_repo),
+    organization_repo: FightOrganizationRepo = Depends(get_boxer_org_repo),
+    rank_repo: RankRepo = Depends(get_rank_repo),
+    weight_class_repo: WeightClassRepo = Depends(get_weight_class_repo)
+) -> BoxerScraperService:
+    return BoxerScraperService(
+        scraper=scraper,
+        fighter_repo=fighter_repo,
+        organization_repo=organization_repo,
+        rank_repo=rank_repo,
+        weight_class_repo=weight_class_repo
+    )
+
+
+def get_boxing_fight_card_service(
+    scraper: BoxingFightCardScraper = Depends(get_boxing_fight_card_scraper),
+    fighter_repo: FighterRepo = Depends(get_boxer_repo),
+    weight_class_repo: WeightClassRepo = Depends(get_weight_class_repo),
+    fight_card_repo: FightCardRepo = Depends(get_fight_card_repo),
+) -> BoxingFightCardService:
+    return BoxingFightCardService(
+        scraper=scraper,
+        fighter_repo=fighter_repo,
+        weight_class_repo=weight_class_repo,
+        fight_card_repo=fight_card_repo
+    )
--- sbtb/fighter/routes.py
+++ sbtb/fighter/routes.py
@@ -3,15 +3,11 @@ from typing import List
 from fastapi import Depends
 from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse, Response
-from fastapi.encoders import jsonable_encoder
 
-from sbtb.fighter.dependencies import get_boxer_repo, get_boxing_scraper, get_boxer_org_repo, get_rank_repo, \
-    get_weight_class_repo, get_boxing_fight_card_scraper, get_fight_card_repo
+from sbtb.fighter.dependencies import get_boxer_scraper_service, get_boxing_fight_card_service
 from sbtb.fighter.schemas import FightCardRead, RankRead
 from sbtb.fighter.service import BoxerScraperService, BoxingFightCardService
-from sbtb.fighter.domain import RankDomain, FightCardDomain
-from sbtb.fighter.repository import FighterRepo, RankRepo, FightOrganizationRepo, WeightClassRepo, FightCardRepo
-from sbtb.fighter.scraper import BoxingRankScraper, BoxingFightCardScraper
+from sbtb.fighter.domain import FightCardDomain
 
 router = APIRouter(prefix="/fighter")
 
@@ -30,39 +26,17 @@ async def fighter_root() -> Response:
             response_description="Update boxing ranks",
             response_model=List[RankRead],
             tags=["fighters"])
-async def scrape_and_save_boxing_ranks(scraper: BoxingRankScraper = Depends(get_boxing_scraper),
-                                       fighter_repo: FighterRepo = Depends(get_boxer_repo),
-                                       organization_repo: FightOrganizationRepo = Depends(get_boxer_org_repo),
-                                       rank_repo: RankRepo = Depends(get_rank_repo),
-                                       weight_class_repo: WeightClassRepo = Depends(get_weight_class_repo)
-                                       ) -> Response:
-    service = BoxerScraperService(scraper=scraper, fighter_repo=fighter_repo,
-                                  organization_repo=organization_repo,
-                                  rank_repo=rank_repo,
-                                  weight_class_repo=weight_class_repo)
+async def scrape_and_save_boxing_ranks(service: BoxerScraperService = Depends(get_boxer_scraper_service)) -> Response:
     updated_ranks: List[RankRead] = await service.scrape_and_update_boxing_ranks()
-    return JSONResponse(
-        content={"updated_ranks": jsonable_encoder(updated_ranks)},
-        status_code=status.HTTP_200_OK,
-    )
+    return updated_ranks
 
 
 @router.get("/update-boxing-fight-cards",
             response_description="Update boxing fight cards",
             response_model=List[FightCardRead],
             tags=["fighters"])
-async def scrape_and_save_boxing_fight_cards(scraper: BoxingFightCardScraper = Depends(get_boxing_fight_card_scraper),
-                                             fighter_repo: FighterRepo = Depends(get_boxer_repo),
-                                             weight_class_repo: WeightClassRepo = Depends(get_weight_class_repo),
-                                             fight_card_repo: FightCardRepo = Depends(get_fight_card_repo)
+async def scrape_and_save_boxing_fight_cards(service: BoxingFightCardService = Depends(get_boxing_fight_card_service),
                                              ) -> Response:
-    service = BoxingFightCardService(scraper=scraper,
-                                     fighter_repo=fighter_repo,
-                                     weight_class_repo=weight_class_repo,
-                                     fight_card_repo=fight_card_repo)
     updated_fight_cards: List[FightCardDomain] = await service.scrape_and_update_boxing_fight_cards()
     fight_card_reads = [FightCardRead.model_validate(fc) for fc in updated_fight_cards]
-    return JSONResponse(
-        content={"updated_boxing_fight_cards": jsonable_encoder(fight_card_reads)},
-        status_code=status.HTTP_200_OK,
-    )
+    return fight_card_reads
